b393da4f0567efe9bda0faadae1b24b8bbcdf696
LuisRemolina <luischuscano1@gmail.com>
2022-08-10 14:43:30 -0500

    add initial configuration spring in gradle

---
+++ src/test/java/com/spring/api/apirestspringboot/ApirestSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.api.apirestspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApirestSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
